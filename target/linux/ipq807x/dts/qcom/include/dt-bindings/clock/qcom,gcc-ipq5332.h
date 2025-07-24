c7c14f952220afb3188a2fb5348c2525414a02b2
gsy-tt <mailgsy@163.com>
2025-07-24 23:18:26 +0800

    Create qcom,gcc-ipq5332.h

---
+++ target/linux/ipq807x/dts/qcom/include/dt-bindings/clock/qcom,gcc-ipq5332.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __DT_BINDINGS_CLOCK_QCOM_GCC_IPQ5332_H
+#define __DT_BINDINGS_CLOCK_QCOM_GCC_IPQ5332_H
+
+/* 时钟标识符（根据你的设备树引用补充） */
+#define GCC_UART0_CLK               1
+#define GCC_USB30_MASTER_CLK        2
+#define GCC_USB30_SLEEP_CLK         3
+#define GCC_ETH0_CLK                4
+#define GCC_PCIE0_CLK               5
+#define GCC_PCIE0_AHB_CLK           6
+#define GCC_SDCC1_AHB_CLK           7
+#define GCC_SDCC1_CLK               8
+
+#endif /* __DT_BINDINGS_CLOCK_QCOM_GCC_IPQ5332_H */
