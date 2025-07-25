35ea827d749b83aee8019c4947e638f38f6e9fe4
gsy-tt <mailgsy@163.com>
2025-07-25 15:05:27 +0800

    Update qcom,gcc-ipq5332.h

--- target/linux/ipq807x/dts/qcom/dt-bindings/clock/qcom,gcc-ipq5332.h
+++ target/linux/ipq807x/dts/qcom/dt-bindings/clock/qcom,gcc-ipq5332.h
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 #ifndef __DT_BINDINGS_CLOCK_QCOM_GCC_IPQ5332_H
 #define __DT_BINDINGS_CLOCK_QCOM_GCC_IPQ5332_H
-
 #define GCC_UART0_CLK               1
 #define GCC_USB30_MASTER_CLK        2
 #define GCC_USB30_SLEEP_CLK         3
@@ -10,5 +9,4 @@
 #define GCC_PCIE0_AHB_CLK           6
 #define GCC_SDCC1_AHB_CLK           7
 #define GCC_SDCC1_CLK               8
-
 #endif /* __DT_BINDINGS_CLOCK_QCOM_GCC_IPQ5332_H */
